d5816b9ac9ed6c555627a35e35aadf275a19bef3
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2020-08-04 21:10:08 -0300

    simple example

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,20 @@
-console.log('Hello Word');
+import express from 'express';
+
+const app = express();
+
+app.get('/users', (request, response) => {
+  const users = [
+    {
+      name: 'Myke Albuquerque Pinto de Oliveira',
+      age: 32
+    },
+    {
+      name: 'Valdemar',
+      age: 41
+    }
+  ];
+  
+  return response.json(users);
+});
+
+app.listen(3333);
